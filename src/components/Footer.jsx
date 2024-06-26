9e687aee85a562679aff0005a4104d8a808d4b0b
Rajeev Ranjan <rajeevranjan.22mca@kongu.edu>
2024-06-26 15:00:58 +0530

    Bug fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
-const Footer =()=>{
-    return (
-        <>
-        <h1>Footer section...</h1>
-        </>
-    )
-}
+const Footer = () => {
+  return (
+    <>
+      <h1>Footer section...</h1>
+      <div className="footer-main">
+        <ul>
+          <li>Term</li>
+          <li>Copy Rights</li>
+          <li>{new Date.now()}</li>
+          <li>Etc</li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
 const Navbar = () => {
+  const listStyle = {
+    padding : "2rem",
+    "list-style": "none"
+  }
   return (
     <>
+    <div className="main" style={{display : "flex"}}>
       <h1>Navbar</h1>
+      <ul style={{display : "flex", padding : "2rem"}}>
+        <li style={listStyle} >Home</li>
+        <li style={listStyle}>About</li>
+        <li style={listStyle}>Contact</li>
+        <li style={listStyle}>Help</li>
+      </ul>
+    </div>
     </>
   );
 };
